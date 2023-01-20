bd1266a9d25165220d3a5da1fbb173808ef3e52e
Nikita <ncheusov@inbox.ru>
2023-01-21 01:13:06 +0300

    Чуть поправил код

--- Lesson_1/GuessTheNumberGame.java
+++ Lesson_1/GuessTheNumberGame.java
@@ -2,14 +2,14 @@ public class GuessTheNumberGame {
 
     public static void main(String[] args) {
         int computerNumber = 11;
-        int playerNumber = 50;
+        int playerNumber = 100;
         for (int i = 1; i <= 1000; i++) {
             if (playerNumber == computerNumber) {
                 System.out.println("Число - " + computerNumber + " Вы победили!");
                 break;
             } else if (playerNumber < computerNumber) {
                 // в диапазоне от 0
-                if (playerNumber < 0) {
+                if (playerNumber <= 0) {
                     playerNumber += 21;
                 } else {
                     System.out.println("Число " + playerNumber + " меньше того, что загадал компьютер");
@@ -17,7 +17,7 @@ public class GuessTheNumberGame {
                 }
             } else if (playerNumber >= computerNumber) {
                 // до 100
-                if (playerNumber > 100) {
+                if (playerNumber >= 100) {
                     playerNumber -= 26;
                 } else {
                     System.out.println("Число " + playerNumber + " больше того, что загадал компьютер");
