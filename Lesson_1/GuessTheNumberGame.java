7f811ca830c3e5e382f9893bc03883768608089e
Nikita <ncheusov@inbox.ru>
2023-01-21 00:45:14 +0300

    Поправил решение по второй задаче

--- Lesson_1/GuessTheNumberGame.java
+++ Lesson_1/GuessTheNumberGame.java
@@ -5,14 +5,24 @@ public class GuessTheNumberGame {
         int playerNumber = 50;
         for (int i = 1; i <= 1000; i++) {
             if (playerNumber == computerNumber) {
-                System.out.println("Вы победили!");
+                System.out.println("Число - " + computerNumber + " Вы победили!");
                 break;
-            } else if (playerNumber <= computerNumber) {
-                System.out.println("Число " + playerNumber + " меньше того, что загадал компьютер");
-                playerNumber++;
+            } else if (playerNumber < computerNumber) {
+                // в диапазоне от 0
+                if (playerNumber < 0) {
+                    playerNumber += 21;
+                } else {
+                    System.out.println("Число " + playerNumber + " меньше того, что загадал компьютер");
+                    playerNumber += 24;
+                }
             } else if (playerNumber >= computerNumber) {
-                System.out.println("Число " + playerNumber + " больше того, что загадал компьютер");
-                playerNumber--;
+                // до 100
+                if (playerNumber > 100) {
+                    playerNumber -= 26;
+                } else {
+                    System.out.println("Число " + playerNumber + " больше того, что загадал компьютер");
+                    playerNumber -= 31;
+                }
             }
         }
     }
