c8ea4c35ac5846f82444ebdf485c435021193686
Nikita <ncheusov@inbox.ru>
2023-01-23 19:59:27 +0300

    Исправил замечания

--- Lesson_1/GuessNumberGame.java
+++ Lesson_1/GuessNumberGame.java
@@ -1,28 +1,28 @@
-public class GuessTheNumberGame {
+public class GuessNumberGame {
 
     public static void main(String[] args) {
         int computerNumber = 11;
         int playerNumber = 50;
         for (int i = 1; i <= 1000; i++) {
-            if (playerNumber == computerNumber) {
-                System.out.println("Число - " + computerNumber + " Вы победили!");
-                break;
-            } else if (playerNumber < computerNumber) {
+            if (playerNumber < computerNumber) {
                 // в диапазоне от 0
-                if (playerNumber <= 0) {
+                if (playerNumber < 1) {
                     playerNumber += 21;
                 } else {
                     System.out.println("Число " + playerNumber + " меньше того, что загадал компьютер");
                     playerNumber += 24;
                 }
-            } else if (playerNumber >= computerNumber) {
+            } else if (playerNumber > computerNumber) {
                 // до 100
-                if (playerNumber >= 100) {
+                if (playerNumber > 100) {
                     playerNumber -= 26;
                 } else {
                     System.out.println("Число " + playerNumber + " больше того, что загадал компьютер");
                     playerNumber -= 31;
                 }
+            } else if (playerNumber == computerNumber) {
+                System.out.println("Число - " + computerNumber + ". \nВы победили!");
+                break;
             }
         }
     }
