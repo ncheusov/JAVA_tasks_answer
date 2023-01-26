9b48d9a64ac0348be80bf7696e8f74c392bdae30
Nikita <ncheusov@inbox.ru>
2023-01-26 23:17:34 +0300

    Переименовал имя класса на Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String sex = "male";
+    String name = "John";
+    float height = 1.80f;
+    int weight = 60;
+    int age = 26;
+
+    String walk() {
+        return "walking";
+    }
+
+    boolean sit() {
+        return false;
+    }
+
+    void run() {
+        System.out.println("running");
+    }
+
+    boolean talk() {
+        return true;
+    }
+
+    void learn() {
+        System.out.println("learning");
+    }
+}
