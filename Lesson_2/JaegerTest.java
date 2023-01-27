5d6588c2e14aee6f73652a4366fd46b3511ef5ce
Nikita <ncheusov@inbox.ru>
2023-01-27 22:59:11 +0300

    Заменил символ

--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -2,9 +2,9 @@ public class JaegerTest {
 
     public static void main(String[] args) {
         Jaeger jaegerOne = new Jaeger();
-        System.out.println("moderName = " + jaegerOne.getModelName());
-        jaegerOne.setModelName("Java");
-        System.out.println("moderName = " + jaegerOne.getModelName());
+        System.out.println("modelName = " + jaegerOne.getModelName());
+        jaegerOne.setModelName("");
+        System.out.println("modelName = " + jaegerOne.getModelName());
         // System.out.println("mark = " + jaegerOne.mark);
         // System.out.println("strength = " + jaegerOne.strength);
         // System.out.println(jaegerOne.drift());
