cb06ffbb468038971f8e570c793d3cc5e89d6da2
Nikita <ncheusov@inbox.ru>
2023-02-08 13:22:35 +0300

    Модифицировал класс Jaeger

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -1,24 +1,123 @@
 public class Jaeger {
 
-    private String modelName = "Bracer Phoenix";
-    private String mark = "Mark-5";
-    private String origin = "USA";
-    private float height = 70.7f;
-    private float weight = 2.1f;
-    private int strength = 8;
-    private int armor = 9;
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int speed;
+    private int strength;
+    private int armor;
 
-    // get - аксессор
-    public String getModelName() {
-        return modelName;
+    public Jaeger() {
+
     }
 
-    // set - мутатор
-    void setModelName(String modelName) {
+    public Jaeger(String modelName, String mark, String origin, float height, float weight, int speed, int strength, int armor) {
         if (modelName == "") {
             System.out.println("Пустая строка");
         } else {
             this.modelName = modelName;
+        }
+        this.mark = mark;
+        this.origin = origin;
+        this.height = height;
+        this.weight = weight;
+        this.strength = strength;
+        this.armor = armor;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        if (modelName == "") {
+            System.out.println("Пустая строка");
+        } else {
+            this.modelName = modelName;
+        }
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        if (mark == "") {
+            System.out.println("Пустая строка");
+        } else {
+            this.mark = mark;
+        }
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        if (height <= 0) {
+            System.out.println("Высота робота не может быть ниже или равна нулю!");
+        } else {
+            this.height = height;
+        }
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        if (weight <= 0) {
+            System.out.println("Вес робота не может быть ниже или равен нулю!");
+        } else {
+            this.weight = weight;
+        }
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        if (speed <= 0) {
+            System.out.println("Скорость не может быть меньше или равна нулю!");
+        } else {
+            this.speed = speed;
+        }
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        if (strength <= 0) {
+            System.out.println("Сила не может быть меньше или равна нуля!");
+        } else if (strength > 10) {
+            System.out.println("Некоректное значение для силы");
+        } else {
+            this.strength = strength;
+        }
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        if (armor <= 0) {
+            System.out.println("Броня не может быть меньше нуля!");
+        } else {
+            this.armor = armor;
         }
     }
 
@@ -27,15 +126,16 @@ public class Jaeger {
         return true;
     }
 
-    public void move() {
-        System.out.println("moving");
+    public void move(String modelName) {
+        this.modelName = modelName;
+        System.out.println(modelName + " moving");
     }
 
     public String scanKaiju() {
         return "scanning";
     }
 
-    public void useVortexCannon() {
-        System.out.println("activated");
+    public void useWeapon(String weapon) {
+        System.out.println(weapon + " activated");
     }
 }
--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -2,11 +2,45 @@ public class JaegerTest {
 
     public static void main(String[] args) {
         Jaeger jaegerOne = new Jaeger();
-        System.out.println("modelName = " + jaegerOne.getModelName());
-        jaegerOne.setModelName("");
-        System.out.println("modelName = " + jaegerOne.getModelName());
-        // System.out.println("mark = " + jaegerOne.mark);
-        // System.out.println("strength = " + jaegerOne.strength);
-        // System.out.println(jaegerOne.drift());
+        Jaeger jaegerTwo = new Jaeger("Bracer Pheonix", "Mark-3", "USA", 79.25f, 1.980f, 7, 8, 6);
+
+        jaegerOne.setModelName("Crimson Typhoon");
+        System.out.println("Имя: " + jaegerOne.getModelName());
+        jaegerOne.setMark("Mark-4");
+        System.out.println("Модель: " + jaegerOne.getMark());
+        jaegerOne.setOrigin("China");
+        System.out.println("Страна: " + jaegerOne.getOrigin());
+        jaegerOne.setHeight(76.2f);
+        System.out.println("Высота: " + jaegerOne.getHeight() + " метров");
+        jaegerOne.setWeight(1.722f);
+        System.out.println("Вес: " + jaegerOne.getWeight() + " тонн");
+        jaegerOne.setSpeed(9);
+        System.out.println("Скорость: " + jaegerOne.getSpeed());
+        jaegerOne.setStrength(8);
+        System.out.println("Сила: " + jaegerOne.getStrength());
+        jaegerOne.setArmor(6);
+        System.out.println("Броня: " + jaegerOne.getArmor());
+        jaegerOne.drift();
+        jaegerOne.move("Crimson Typhoon");
+        jaegerOne.scanKaiju();
+        jaegerOne.useWeapon("Plasmacaster");
+
+        System.out.println("\nБыл: " + jaegerTwo.getModelName());
+        jaegerTwo.setModelName("Gipsy Danger");
+        System.out.println("Стал: " + jaegerTwo.getModelName());
+        System.out.println("Модель: " + jaegerTwo.getMark());
+        System.out.println("Страна: " + jaegerTwo.getOrigin());
+        System.out.println("Высота: " + jaegerTwo.getHeight() + " метров");
+        System.out.println("Вес: " + jaegerTwo.getWeight() + " тонн");
+        jaegerTwo.setSpeed(8);
+        System.out.println("Скорость: " + jaegerTwo.getSpeed());
+        jaegerTwo.setStrength(9);
+        System.out.println("Сила: " + jaegerTwo.getStrength());
+        jaegerTwo.setArmor(4);
+        System.out.println("Броня: " + jaegerTwo.getArmor());
+        jaegerTwo.drift();
+        jaegerTwo.move("Gipsy Danger");
+        jaegerTwo.scanKaiju();
+        jaegerTwo.useWeapon("Chain Sword");
     }
 }
