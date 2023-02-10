40607802910f365aaa92032d5c29c9f606fc4300
Nikita <ncheusov@inbox.ru>
2023-02-10 19:34:17 +0300

    Исправил оператор ветвления в обработке ответа пользователя

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -6,19 +6,20 @@ public class CalculatorTest {
         Calculator myCalculator = new Calculator();
         myCalculator.start();
         Scanner scan = new Scanner(System.in);
-        System.out.print("Хотите продолжить вычисления? [yes/no]");
+        System.out.println("Хотите продолжить вычисления? [yes/no]");
         String answer = scan.nextLine();
         while (true) {
             if (answer.equals("yes")) {
                 myCalculator.start();
-                System.out.print("Хотите продолжить вычисления? [yes/no]");
+                System.out.println("Хотите продолжить вычисления? [yes/no]");
                 answer = scan.nextLine();
-            } else if (answer.equals("no")) {
+            } else {
+                System.out.println("Некорректное значение! Вводите с клавиатуры только yes или no");
+                answer = scan.nextLine();
+            }
+            if (answer.equals("no")) {
                 scan.close();
                 break;
-            } else {
-                System.out.println("Некорректное значение");
-                answer = scan.nextLine();
             }
         }
     }
