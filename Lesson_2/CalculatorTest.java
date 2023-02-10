be3b3742f00b19c895cb694c338b8b2fc5ab0e10
Nikita <ncheusov@inbox.ru>
2023-02-10 19:26:59 +0300

    Поправил обработку ответа пользователя в калькуляторе

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -8,10 +8,18 @@ public class CalculatorTest {
         Scanner scan = new Scanner(System.in);
         System.out.print("Хотите продолжить вычисления? [yes/no]");
         String answer = scan.nextLine();
-        while (answer.equals("yes")) {
-            myCalculator.start();
-            System.out.print("Хотите продолжить вычисления? [yes/no]");
-            answer = scan.nextLine();
+        while (true) {
+            if (answer.equals("yes")) {
+                myCalculator.start();
+                System.out.print("Хотите продолжить вычисления? [yes/no]");
+                answer = scan.nextLine();
+            } else if (answer.equals("no")) {
+                scan.close();
+                break;
+            } else {
+                System.out.println("Некорректное значение");
+                answer = scan.nextLine();
+            }
         }
     }
 }
