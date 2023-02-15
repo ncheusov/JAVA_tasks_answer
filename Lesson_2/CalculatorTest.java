74b8756e73f08e3f5a8f50fa28aa46898c9b6737
Nikita <ncheusov@inbox.ru>
2023-02-15 18:18:46 +0300

    Исправил замечания

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -5,36 +5,27 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator myCalculator = new Calculator();
         Scanner scan = new Scanner(System.in);
-        while (true) {
-            System.out.print("Введите первое число: ");
-            myCalculator.setNum1(scan.nextInt());
+        String answer = "yes";
+        while (!answer.equals("no")) {
+            if (answer.equals("yes")) {
+                System.out.print("Введите первое число: ");
+                myCalculator.setNum1(scan.nextInt());
 
-            System.out.print("Введите знак математической операции: ");
-            scan.nextLine();
-            myCalculator.setSign(scan.nextLine().charAt(0));
+                System.out.print("Введите знак математической операции: ");
+                scan.nextLine();
+                myCalculator.setSign(scan.nextLine().charAt(0));
 
-            System.out.print("Введите второе число: ");
-            myCalculator.setNum2(scan.nextInt());
-            myCalculator.calculate();
+                System.out.print("Введите второе число: ");
+                myCalculator.setNum2(scan.nextInt());
+                myCalculator.calculate();
 
-            System.out.println("Хотите продолжить вычисления? [yes/no]");
-            scan.nextLine();
-            String answer = scan.nextLine();
-            while (!answer.equals("yes") && !answer.equals("no")) {
+                System.out.print("Хотите продолжить вычисления? [yes/no] ");
+                scan.nextLine();
+                answer = scan.nextLine();
+            } else if (!answer.equals("yes") && !answer.equals("no")) {
                 System.out.print("Пожалуйста, введите yes или no: ");
                 answer = scan.nextLine();
-                if (answer.equals("yes")) {
-                    continue;
-                } else if (answer.equals("no")) {
-                    break;
-                }
             }
-            if (answer.equals("yes")) {
-                continue;
-            } else if (answer.equals("no")) {
-                break;
-            }
-            scan.close();
         }
     }
 }
