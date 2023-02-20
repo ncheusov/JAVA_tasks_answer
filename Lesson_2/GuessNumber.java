21359e850bdff67f8d907ae3b26e5285de8cf562
Nikita <ncheusov@inbox.ru>
2023-02-21 00:54:36 +0300

    Исправил замечания

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,34 +1,41 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessNumber {
 
-    private int computerNumber = (int) Math.round(Math.random() * 100);
     private Scanner scan = new Scanner(System.in);
+    private Player player1 = new Player();
+    private Player player2 = new Player();
 
-    public void start(Player player1, Player player2) {
+    public GuessNumber(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public void start() {
+        Random randInt = new Random();
+        int computerNumber = randInt.nextInt(100) + 1;
         while (true) {
             System.out.print(player1.getName() + ", угадай число: ");
             player1.setNumber(scan.nextInt());
-            if (player1.getNumber() < computerNumber) {
+            if (player1.getNumber() == computerNumber) {
+                System.out.println(player1.getName() + " победил");
+                break;
+            } else if (player1.getNumber() < computerNumber) {
                 System.out.println("Число " + player1.getNumber() + " меньше того, что загадал компьютер");
             } else if (player1.getNumber() > computerNumber) {
                 System.out.println("Число " + player1.getNumber() + " больше того, что загадал компьютер");
-            } else if (player1.getNumber() == computerNumber) {
-                System.out.println(player1.getName() + " победил");
-                computerNumber = (int) Math.round(Math.random() * 100);
-                break;
             }
 
             System.out.print(player2.getName() + ", угадай число: ");
             player2.setNumber(scan.nextInt());
-            if (player2.getNumber() < computerNumber) {
+            if (player2.getNumber() == computerNumber) {
+                System.out.println(player2.getName() + " победил");
+                break;
+            } else if (player2.getNumber() < computerNumber) {
                 System.out.println("Число " + player2.getNumber() + " меньше того, что загадал компьютер");
             } else if (player2.getNumber() > computerNumber) {
                 System.out.println("Число " + player2.getNumber() + " больше того, что загадал компьютер");
-            } else if (player2.getNumber() == computerNumber) {
-                System.out.println(player2.getName() + " победил");
-                computerNumber = (int) Math.round(Math.random() * 100);
-                break;
             }
         }
     }
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -4,15 +4,15 @@ public class GuessNumberTest {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        GuessNumber game = new GuessNumber();
+        System.out.println("Имя первого игрока: ");
+        Player player1 = new Player(scan.next());
+        System.out.println("Имя второго игрока: ");
+        Player player2 = new Player(scan.next());
+        GuessNumber game = new GuessNumber(player1, player2);
         String answer = "yes";
         while (!answer.equals("no")) {
             if (answer.equals("yes")) {
-                System.out.println("Имя первого игрока: ");
-                Player player1 = new Player(scan.next());
-                System.out.println("Имя второго игрока: ");
-                Player player2 = new Player(scan.next());
-                game.start(player1, player2);
+                game.start();
             }
             System.out.println("Хотите продолжить игру? [yes/no]");
             scan.nextLine();
