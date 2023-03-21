1b7d763835f37b068f1374700dc60543122e41f9
Nikita <ncheusov@inbox.ru>
2023-03-21 21:16:43 +0300

    "Исправил 6-ю и 5-ю задачи, избавился от дублирования кода"

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -9,9 +9,9 @@ public class ArrayTheme {
         System.out.print("Массив до модификации: ");
         printArr(intArr);
         System.out.println();
-        for (int i = 0; i < len--; i++) {
+        for (int i = 0; i < len; i++) {
             int tmp = intArr[i];
-            intArr[i] = intArr[len];
+            intArr[i] = intArr[--len];
             intArr[len] = tmp;
         }
         System.out.print("Массив после модификации: ");
@@ -33,24 +33,24 @@ public class ArrayTheme {
 
         System.out.println("\n3. Удаление элементов массива");
         double[] doubleArr = new double[15];
-        double[] modifiedArr = new double[15];
         len = doubleArr.length;
-        int zeroes = 0;
         for (int i = 0; i < len; i++) {
             doubleArr[i] = Math.random();
-            modifiedArr[i] = doubleArr[i];
-            int middleIndex = len / 2;
-            if (modifiedArr[i] > modifiedArr[middleIndex]) {
-                modifiedArr[i] = 0;
-                if (modifiedArr[i] == 0) {
-                    zeroes += 1;
-                }
-            }
         }
         System.out.println("Массив до модификации");
         printFormattedArr(doubleArr);
+        int zeroes = 0;
+        for (int i = 0; i < len; i++) {
+            int middleIndex = len / 2;
+            if (doubleArr[i] > doubleArr[middleIndex]) {
+                doubleArr[i] = 0;
+                if (doubleArr[i] == 0) {
+                    zeroes++;
+                }
+            }
+        }
         System.out.println("\nМассив после модификации");
-        printFormattedArr(modifiedArr);
+        printFormattedArr(doubleArr);
         System.out.println("\nКоличество обнуленных ячеек: " + zeroes);
 
         System.out.println("\n4. Вывод элементов массива лесенкой в обратном порядке");
@@ -69,18 +69,15 @@ public class ArrayTheme {
         System.out.println("\n5. Генерация уникальных чисел");
         intArr = new int[30];
         len = intArr.length;
+        int randomNum = 0;
         for (int i = 0; i < len; i++) {
-            intArr[i] = (60 + (int) (Math.random() * 40));
-        }
-        for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j < len; j++) {
-                while (intArr[i] == intArr[j]) {
-                    intArr[j] = (60 + (int) (Math.random() * 40));
-                }
+            while (contains(intArr, randomNum)) {
+                randomNum = (60 + (int) (Math.random() * 40));
             }
+            intArr[i] = randomNum;
         }
         for (int i = 0; i < len - 1; i++) {
-            for (int j = 0; j < len - 1; j++) {
+            for (int j = 0; j < len - 1 - i; j++) {
                 if (intArr[j] > intArr[j + 1]) {
                     int tmp = intArr[j];
                     intArr[j] = intArr[j + 1];
@@ -98,16 +95,19 @@ public class ArrayTheme {
         System.out.println("\n6. Копирование не пустых строк");
         String[] stringsArr1 = {"    ", "AA", "", "BBB", "CC", "D", "    ", "E", "FF", "G", ""};
         len = stringsArr1.length;
-        int index = 0;
+        int count = 0;
         for (int i = 0; i <= len - 1; i++) {
             if (stringsArr1[i].isBlank()) {
-                index++;
+                count++;
             }
         }
-        String[] stringsArr2 = new String[len - index];
-        System.arraycopy(stringsArr1, 1, stringsArr2, 0, 1);
-        System.arraycopy(stringsArr1, 3, stringsArr2, 1, 3);
-        System.arraycopy(stringsArr1, 7, stringsArr2, 4, 3);
+        String[] stringsArr2 = new String[len - count];
+        int j = 0;
+        for (int i = 0; i < len; i++) {
+            if (!stringsArr1[i].isBlank()) {
+                System.arraycopy(stringsArr1, i, stringsArr2, j++, 1);
+            }
+        }
         for (String str : stringsArr1) {
             if (str.isBlank()) {
                 System.out.print(str);
@@ -134,5 +134,14 @@ public class ArrayTheme {
                 System.out.println();
             }
         }
+    }
+
+    private static boolean contains(int[] arr, int num) {
+        for (int digit : arr) {
+            if (digit == num) {
+                return true;
+            }
+        }
+        return false;
     }
 }
