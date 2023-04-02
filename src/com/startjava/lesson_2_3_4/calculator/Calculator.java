7fec0f8d5b42f675975d62562ed5a3358e678e07
Nikita <ncheusov@inbox.ru>
2023-04-02 22:43:01 +0300

    "Сделал задания со звездочкой кроме последних трех"

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,26 +2,21 @@ package startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public double calculate(String mathExpression) {
+    public static double calculate(String mathExpression) {
         String[] expression = mathExpression.split(" ");
         int num1 = Integer.parseInt(expression[0]);
         String sign = expression[1];
         int num2 = Integer.parseInt(expression[2]);
-        double result = 0;
-        switch (sign) {
-            case "+":
-                return Math.addExact(num1, num2);
-            case "-":
-                return Math.subtractExact(num1, num2);
-            case "*":
-                return Math.multiplyExact(num1, num2);
-            case "/":
-                return Math.floorDiv(num1, num2);
-            case "%":
-                return Math.floorMod(num1, num2);
-            case "^":
-                return Math.pow(num1, num2);
-        }
+        double result = switch (sign) {
+            case "+" -> Math.addExact(num1, num2);
+            case "-" -> Math.subtractExact(num1, num2);
+            case "*" -> Math.multiplyExact(num1, num2);
+            case "/" -> Math.floorDiv(num1, num2);
+            case "%" -> Math.floorMod(num1, num2);
+            case "^" -> Math.pow(num1, num2);
+            default ->
+                throw new RuntimeException("Некореткный знак");
+        };
         return result;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -2,16 +2,17 @@ package startjava.lesson_2_3_4.calculator;
 
 import java.util.Scanner;
 
+import static startjava.lesson_2_3_4.calculator.Calculator.calculate;
 public class CalculatorTest {
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
         Scanner scan = new Scanner(System.in);
         String answer = "yes";
         while (!answer.equals("no")) {
             if (answer.equals("yes")) {
                 System.out.print("Введите математическое выражение: ");
-                printResult(calculator.calculate(scan.nextLine()));
+                String mathExpression = scan.nextLine();
+                printResult(calculate(mathExpression));
             }
             System.out.println("\nХотите продолжить вычисления? [yes/no] ");
             answer = scan.nextLine();
@@ -19,10 +20,6 @@ public class CalculatorTest {
     }
 
     private static void printResult(double result) {
-        if (result % 1 != 0) {
-            System.out.printf("%.3f", result);
-        } else {
-            System.out.printf("%.0f", result);
-        }
+        System.out.printf((result % 1 != 0) ? "%3.f" : "%.0f", result);
     }
 }
