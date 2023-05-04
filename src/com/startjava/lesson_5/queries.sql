daa2f7b786aafa63f034550ecdabff9319c0c81a
Nikita <ncheusov@inbox.ru>
2023-05-04 19:28:32 +0300

    "Отформатировал запросы, исправил ошибки"

--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -6,37 +6,32 @@ SELECT * FROM jaegers
  WHERE status = 'Active';
 
 \echo '-- print Mark-1 and Mark-4'
-SELECT modelName,
-       mark
+SELECT modelName, mark
   FROM jaegers
  WHERE mark IN (1, 4);
 
 \echo '-- print all robots except Mark-1 and Mark-4'
-SELECT modelName,
-       mark
+SELECT modelName, mark
   FROM jaegers
  WHERE mark NOT IN (1, 4);
 
 \echo '-- order robots by mark in decreasing'
 SELECT * FROM jaegers
- ORDER BY mark DESC;
+ ORDER BY mark
+ DESC;
 
 \echo '-- info about oldest robot'
 SELECT * FROM jaegers
- WHERE launch =
-       (SELECT MIN(launch)
-          FROM jaegers);
+ WHERE launch = (SELECT MIN(launch)
+  FROM jaegers);
 
 \echo '-- robots that destroyed the most kaiju'
-SELECT modelName,
-       kaijuKill,
-       mark
+SELECT modelName, kaijuKill, mark
   FROM jaegers
- WHERE kaijuKill =
-       (SELECT MAX(kaijuKill)
-          FROM jaegers);
+ WHERE kaijuKill = (SELECT MAX(kaijuKill)
+  FROM jaegers);
 
-\echo '-- average weigth of robots'
+\echo '-- average weight of robots'
 SELECT AVG(weight)
   FROM jaegers;
 
